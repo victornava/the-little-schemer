5bfb7d70cf9f1ee2038ad4ed07b7d39a8cc1608a
Victor Nava <victornavav@gmail.com>
2017-02-24 22:12:36 +1100

    Chapter 7

--- the-little-schemer.rkt
+++ the-little-schemer.rkt
@@ -635,3 +635,223 @@
 (value `(- 5 2))
 (value `(x 5 2))
 (value `(+ (x 3 6) (- 8 2)))
+
+; ============================================================================
+
+(define set?
+  (lambda (lat)
+    (cond
+      ((null? lat) #t)
+      ((member? (car lat) (cdr lat)) #f)
+      (else
+        (set? (cdr lat))))))
+
+(set? `())
+(set? `(apple))
+(set? `(apple peaches apple plum))
+(set? `(apples peaches pears plums))
+
+; ============================================================================
+
+(define makeset
+  (lambda (lat)
+    (cond
+      ((null? lat)`())
+      (else
+        (cons
+          (car lat)
+          (makeset
+            (multirember (car lat) (cdr lat))))))))
+
+(makeset `(apple peach pear peach plum apple lemon peach))
+(makeset `(apple 3 pear 4 9 apple 3 4))
+
+; ============================================================================
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else
+        (and
+          (member? (car set1) set2)
+          (subset? (cdr set1) set2))))))
+
+(subset?
+  `(5 chicken wings)
+  `(5 hamburgers 2 pieces fried chicken and light duck wings))
+
+(subset?
+  `(5 chicken wings)
+  `(2 pieces fried chicken and light duck wings))
+
+; ============================================================================
+
+(define eqset?
+  (lambda (set1 set2)
+    (and
+      (subset? set1 set2))
+      (subset? set2 set1)))
+
+(eqset?
+  `(6 large chickens with wings)
+  `(6 chickens with large wings))
+
+; ============================================================================
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else
+        (or
+          (member? (car set1) set2)
+          (intersect? (cdr set1) set2))))))
+
+(intersect?
+  `(stewed tomatoes and macaroni)
+  `(macaroni and cheese))
+
+; ============================================================================
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) `())
+      ((member? (car set1) set2)
+        (cons
+          (car set1)
+          (intersect (cdr set1) set2)))
+      (else
+        (intersect (cdr set1) set2)))))
+
+(intersect
+  `(stewed tomatoes and macaroni)
+  `(macaroni and cheese))
+
+; ============================================================================
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+        (union (cdr set1) set2))
+      (else
+        (cons (car set1)
+          (union (cdr set1) set2))))))
+
+(union
+  `(stewed tomatoes and macaroni caserole)
+  `(macaroni and cheese))
+
+; ============================================================================
+
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else
+        (intersect
+          (car l-set)
+          (intersectall (cdr l-set)))))))
+
+(intersectall `((a b c) (c a de) (e f g h a b)))
+
+; ============================================================================
+
+(define a-pair?
+  (lambda (x)
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      ((null? (cdr (cdr x))) #t)
+      (else #f))))
+
+(a-pair? `(pear pear))
+(a-pair? `((2) (pair)))
+
+; ============================================================================
+
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
+
+(fun? `((8 3) (4 2) (7 6) (6 2) (3 4)))
+(fun? `((d 4) (b 0) (b 9) (e 5) (g 4)))
+
+; ============================================================================
+
+(define first
+  (lambda (x)
+    (car x)))
+
+(first `(uno dos))
+(first `((uno) (dos)))
+
+; ============================================================================
+
+(define second
+  (lambda (x)
+    (car (cdr x))))
+
+(second `(uno dos))
+(second `((uno) (dos)))
+
+; ============================================================================
+
+(define build
+  (lambda (s1 s2)
+    (cond
+      (else
+        (cons s1 (cons s2 `()))))))
+
+(build `uno `dos)
+
+; ============================================================================
+
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
+
+(revpair `(uno dos))
+
+; ============================================================================
+
+(define revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) `())
+      (else
+        (cons
+          (revpair (car rel))
+          (revrel (cdr rel)))))))
+
+(revrel `((8 a) (pumpkin pie) (got sick)))
+
+; ============================================================================
+
+(define seconds
+  (lambda (l)
+    (cond
+      ((null? l) `())
+      (else (cons (second (car l))
+              (seconds (cdr l)))))))
+
+(seconds `((1 2) (3 4) (5 6)))
+
+; ============================================================================
+
+(define one-to-one?
+  (lambda (fun)
+    (fun? (revrel fun))))
+
+
+(one-to-one? `((grape raisin)
+               (plum prune)
+               (stewed prune)))
+
+(one-to-one? `((grape raisin)
+               (plum prune)
+               (stewed grape)))
